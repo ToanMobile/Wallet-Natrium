7eb5e81e566348584423ebad692ed255f5f9805a
Văn Toàn <huynhvantoan.itc@gmail.com>
2021-05-20 09:36:17 +0700

    First commit.

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/vantoan/fvm/versions/dev"
+export "FLUTTER_APPLICATION_PATH=/Users/vantoan/Downloads/coin/natrium_wallet_flutter-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=2.4.1"
+export "FLUTTER_BUILD_NUMBER=79"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
